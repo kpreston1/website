be8fb180d296346d022bb0127dfe4b826927a5b0
Kyle Preston <kylepreston@Kyles-MacBook-Pro.local>
2019-02-17 19:11:24 -0600

    initializing git repo

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,23 @@
+package hello;
+
+import hello.Greeter.Greeter;
+import org.joda.time.LocalTime;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Application {
+
+    @RequestMapping("/")
+    String home() {
+        Greeter greeter = new Greeter();
+        return greeter.sayHello()+" The current local time is: " + new LocalTime();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/hello/GreeterTest.java
@@ -0,0 +1,18 @@
+package hello;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+
+import hello.Greeter.Greeter;
+import org.junit.Test;
+
+public class GreeterTest {
+
+    private Greeter greeter = new Greeter();
+
+    @Test
+    public void greeterSaysHello() {
+        assertThat(greeter.sayHello(), containsString("Hello"));
+    }
+
+}
